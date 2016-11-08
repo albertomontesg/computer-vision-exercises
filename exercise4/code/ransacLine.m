931640409591be258d7dc1993a40712f64ae56f0
Alberto Montes <alm59321@gmail.com>
2016-11-08 16:53:37 +0100

    Start exercise

--- exercise4/code/ransacLine.m
+++ exercise4/code/ransacLine.m
@@ -1,4 +1,4 @@
-function [k, b] = ransacLine(data, dim, iter, threshDist, inlierRatio)
+function [k, b] = ransacLine(data, num, iter, threshDist, inlierRatio)
 % data: a 2xn dataset with #n data points
 % num: the minimum number of points. For line fitting problem, num=2
 % iter: the number of iterations
@@ -11,13 +11,39 @@ k=0; b=0;              % parameters for best fitting line
 
 for i=1:iter
     % Randomly select 2 points
-
-    % Compute the distances between all points with the fitting line
-
+    idx = randsample(number, num);
+    p_x = data(1,idx);
+    p_y = data(2,idx);
+    P = polyfit(p_x, p_y, 1);
+    k_i = P(1); b_i = P(2);
+    % Compute the distances between all points with the fitting line    
+    d = (-data(2,:)+k_i*data(1,:)+b_i) / (sqrt(1+k_i*k_i));
+    d = abs(d);
     % Compute the inliers with distances smaller than the threshold
-
+    inNum = sum(d <= threshDist);
+    inIdx = find(d <= threshDist);
+    inRatio = inNum / number;
+    
     % Update the number of inliers and fitting model if better model is found
-
+    if inNum > bestInNum
+        bestInNum = inNum;
+        k = k_i;
+        b = b_i;
+    end
+    if inRatio > inlierRatio
+        p_x = data(1, inIdx);
+        p_y = data(2, inIdx);
+        P = polyfit(p_x, p_y, 1);
+        k = P(1); b = P(2);
+        d = (-data(2,:)+k_i*data(1,:)+b_i) / (sqrt(1+k_i*k_i));
+        d = abs(d);
+        inNum = sum(d <= threshDist);
+        if inNum > bestInNum
+            bestInNum = inNum;
+        end
+    end
+    
+  
 end
 
 end
